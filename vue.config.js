a9aed823c3cc49040e330ecd78dcdf80304b70b7
hhhhhq <Mr.j@tom.com>
2021-01-25 16:33:32 +0800

    add publicPath to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,12 @@
  * @Author: hhhhhq
  * @Date: 2021-01-09 21:01:01
  * @LastEditors: hhhhhq
- * @LastEditTime: 2021-01-23 23:31:59
+ * @LastEditTime: 2021-01-25 16:33:13
  * @Description: file content
  */
 module.exports = {
   lintOnSave: false,
+  publicPath: "/blason-capelviel/",
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       args[0].title = "伽贝徽章"
